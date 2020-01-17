3e79658cbf6259a68c6fbff73c434e3ac604ffa2
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2020-01-17 14:47:46 +0000

    add task for compiling all bibliographies to one

--- tasks.py
+++ tasks.py
@@ -6,3 +6,10 @@ def compile(c):
     Compile the LaTeX document.
     """
     c.run("latexmk  -interaction=nonstopmode  --xelatex -shell-escape main.tex")
+
+@task
+def get_bibliography(c):
+    """
+    Merges the bibliography files for each chapter to one.
+    """
+    c.run("cat src/chapters/01/bibliography.bib >> bibliography.bib")
