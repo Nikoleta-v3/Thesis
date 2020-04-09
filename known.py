9690d8573d52f05a3070f68cfb7d29e825e22620
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2020-04-09 19:29:04 +0100

    pass spellcheck locally

--- known.py
+++ known.py
@@ -211,5 +211,13 @@ words = {
     "Random's",
     "PRNGs",
     "Parallelisation",
-    "TF"
+    "TF",
+    "Arcas",
+    "LSTM",
+    "SymPy",
+    "Zenodo",
+    "devaluating",
+    "impactful",
+    "provocable",
+    "sensei",
 }
