ef0074e3473f475ae8c384f5dfb5f39e4ce41696
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2020-05-02 15:39:32 +0100

    Sequences chapter (#192)
    * adds main notebook to chapter
    
    * adds bibliography to chapter
    
    * adds tex for chapter's diagrams
    
    * compile bibliography
    
    * initial write up of chapter
    
    * comment out chapter 5 to compile
    
    * add to bibliography and compile
    
    * chapter first draft
    
    * adds files susch a diagrams and constants used in the chapter
    
    * add sumodule to chapter
    
    * adds notebook
    
    Note that the notebook makes use of sequence sensei. Thus,
    after cloning the submodule in the enviroment the command
    python setup.py install needs to be run in the submodule
    
    * final revisions to chapter
    
    * create tex folder
    
    * pass spellcheck
    
    * pass doctests
    
    * adds axelrod to requirements
    
    * add specific version of axelrod
    
    * install as a requirement fsspec>=0.3.3
    
    * pass doctest
    
    * re writes and suggestions by vk
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * tweaks to mathematics
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * tweaks and finalises introduction
    
    * commit bug notebook
    
    * delete notebook
    
    * fix genatic algorithm section
    
    * script to check the percentage of wrong sequences in the data set
    
    * compile bibliography
    
    * final script for checking the stochastic strategies.
    
    * clean and add comments to chapter's notebook
    
    * adds tables in tex
    
    * removes failed names as they are none
    
    * add images to chapter
    
    * draft of the chapter
    
    * pass spellcheck
    
    * reads over section 6.2
    
    * reads over section 6.3
    
    * adds plots to data collection section
    
    * number of stochastic and deterministic strategies to variables
    
    * runs chapter's notebook
    
    * fixes parameter's symbol in tables
    
    * reads over data collections and summary sections
    
    * pass doctests
    
    * pass doctest
    
    run code with the correct version of axelrod
    
    * removes todo
    
    * morning tweaks
    
    * adds fsspec to requirements.txt
    
    * vk re writes suggestions
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * adds TODOs
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * rewrites I need to revisit
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * adds space and pass spellcheck
    
    * runs blackbook on main.ipynb and tweaks code snippet
    
    * removes footnote
    
    Co-Authored-By: Vince Knight <vincent.knight@gmail.com>
    
    * plot all sequences instead of subsets
    
    * adds TODO
    
    * removes minor TODO by vk. references chapter
    
    * replaces hardcoded numbers with read in files
    
    * adds a discussion for the 45% data points that are not reproducible
    
    * adds sentence about a non converged sequence being usefull
    
    * adds discussion for succefully captured best response sequences
    
    * pass spellcheck
    
    * increases the width of the sequences diaphical representation plot
    
    * apply v.k. fixes
    
    Co-authored-by: Vince Knight <vincent.knight@gmail.com>
    
    * creates variables for reproducible and non percentage of seq.
    
    Co-authored-by: Vince Knight <vincent.knight@gmail.com>

---
+++ src/chapters/06/check_data.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+import tqdm
+import json
+
+import axelrod as axl
+
+import pandas as pd
+
+import sequence_sensei as ss
+
+
+strategies = {
+    strategy().name: strategy
+    for strategy in axl.strategies
+    if strategy.classifier["stochastic"] == True
+}
+
+genes = ["gene_{}".format(i) for i in range(205)]
+
+
+def run_seed_match_with_repetitions(
+    seed, player, sequence, score, repetitions=5
+):
+    for _ in range(repetitions):
+        axl.seed(seed)
+        match = axl.Match([player(), axl.Cycler(sequence)], turns=205)
+
+        _ = match.play()
+
+        check = score == match.final_score_per_turn()[-1]
+        if check == False:
+            return False
+    return True
+
+
+failed_names = []
+checks = {}
+for file in tqdm.tqdm(Path("/Volumes/ext/Data/raw_data/").glob("*")):
+    file = str(file)
+    if file.split("_")[-1] == "nan":
+        continue
+    df = pd.read_csv(file + "/main.csv")
+    df = df[df["generation"] == 2000]
+    columns = ['score', 'seed', 'opponent'] + genes
+
+    for i, row in df[df["score"] == df["score"].max()][columns].drop_duplicates().reset_index().iterrows():
+        try:
+            opponent = strategies[row["opponent"]]
+        except KeyError:
+            failed_names.append(row["opponent"])
+
+        sequence = ss.get_sequence_str(row[genes].values)
+        score = row["score"]
+        seed = int(row["seed"])
+
+        check = run_seed_match_with_repetitions(
+            seed=seed, player=opponent, sequence=sequence, score=score
+        )
+
+        key = row["opponent"] + "_" + str(seed) + "_best_response_" + str(i)
+        checks[key] = check
+
+with open("checks.txt", "w") as file:
+    file.write(json.dumps(checks))
+
+with open("failed_names.txt", "w") as file:
+    file.write("\n".join(failed_names))
